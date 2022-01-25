67c8b804fe5e6eb1173a2f4d48eb44bf463e9b15
alesyaradevich <alesyaradevich@mail.ru>
2022-01-25 15:47:04 +0200

    Initial project version

---
+++ app.rb
@@ -0,0 +1,37 @@
+require 'pg'
+require 'csv'
+require_relative 'db'
+
+con = DB.new
+con.create_tables
+con.delete_table_content
+con.import_csv_to_table('/home/ubuntu/users1.csv', 'users1')
+con.import_csv_to_table('/home/ubuntu/users2.csv', 'users2')
+con.import_csv_to_table('/home/ubuntu/pictures.csv', 'pictures')
+con.import_csv_to_table('/home/ubuntu/avatars.csv', 'avatars')
+con.import_csv_to_table('/home/ubuntu/user_picture.csv', 'user_picture')
+
+puts "Welcome to application."
+
+loop do
+  puts "What would you like to do with the user data?"
+  puts "Press 1 to view information of the users."
+  puts "Press 2 to view information about users' avatars."
+  puts "Press 3 to view information about user's pictures."
+  puts "Press 0 if you want to stop the work in application"
+  answer = gets.chomp
+  case answer
+  when "1"
+    puts con.select1.entries
+  when "2"
+    puts con.select2.entries
+  when "3"
+    puts "Select user."
+    user_id = gets.chomp
+    puts con.select3(user_id).entries
+  when "0"
+    break
+  else
+    puts "The input is invalid."
+  end
+end
